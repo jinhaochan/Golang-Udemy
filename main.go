606c8462a7cb8110c2202d164019103683675edb
jinhao <jinhao_@hotmail.com>
2021-03-07 22:47:11 +0800

    updated main

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+	// equivalent to `var card string = "Ace of Spades"`
+	card := "Ace of Spades"
+
+	// Golang being a static language, you cannot assign another type to it
+	// such as `card = 1`
+
+	// reassignment of value to card
+	// you cannot run `card := "Five of Diamonds"` again
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
 }
