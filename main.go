c382dabba5f2a9212c28976de64a5c222f158cb0
jinhao <jinhao_@hotmail.com>
2021-03-07 22:51:27 +0800

    updated main

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// declaration of variables can be done outside the functions
+// they just cannot be assigned a value
+// e.g. `var card2 string = "Queen of Hearts"`
+
+var card2 string
+
 func main() {
 	// equivalent to `var card string = "Ace of Spades"`
 	card := "Ace of Spades"
@@ -13,5 +19,7 @@ func main() {
 	// you cannot run `card := "Five of Diamonds"` again
 	card = "Five of Diamonds"
 
-	fmt.Println(card)
+	card2 = "Queen of Hearts"
+
+	fmt.Println(card, " ", card2)
 }
