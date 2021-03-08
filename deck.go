359ff4003421c7afde233153b3a0fc4580fbc542
jinhao <jinhao_@hotmail.com>
2021-03-08 22:46:07 +0800

    updated receiver functions

--- deck.go
+++ deck.go
@@ -6,6 +6,11 @@ import "fmt"
 // which is a slice of string
 type deck []string
 
+// receiver function
+// by declaring the receiver below,
+// any variable of type deck gets access to this print method
+// d is the actual copy of the data we're working with
+// the receiver is traditionally named as the first letter of the type
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
